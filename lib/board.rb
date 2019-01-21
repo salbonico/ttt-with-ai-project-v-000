8b789c082510546e38752773110b675c235815d9
Steve Albonico <salbonico@gmail.com>
2019-01-21 17:26:31 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,14 +1,15 @@
 class Board
 
-attr_accessor :cells 
+attr_accessor :cells
 
 
 def initialize
   @cells = ["","","","","","","","",""]
 end
 
-
-
+def reset
+  self.cells = ["","","","","","","","",""]
+end
 
 
 end
