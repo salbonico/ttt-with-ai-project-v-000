05bf069873b7e47c46106c336373bee7e892af01
Steve Albonico <salbonico@gmail.com>
2019-01-21 17:33:45 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -11,7 +11,13 @@ def reset!
   self.cells = [" "," "," "," "," "," "," "," "," "]
 end
 
-
+def display
+puts " #{self.cells[0]} | #{self.cells[1]} | #{self.cells[2]} "
+puts "-----------"
+puts " #{self.cells[3]} | #{self.cells[4]} | #{self.cells[5]} "
+puts "-----------"
+puts " #{self.cells[6]} | #{self.cells[7]} | #{self.cells[8]} "
+end
 
 
 
